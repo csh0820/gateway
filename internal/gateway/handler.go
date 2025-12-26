8fcb40561828a60d2f77228aa425036dabddf4c9
chaosheng <319419045@qq.com>
2025-12-26 15:38:13 +0800

    gateway reverse proxy

--- internal/gateway/handler.go
+++ internal/gateway/handler.go
@@ -2,7 +2,10 @@ package gateway
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 	"net/http/httputil"
+	"net/url"
 	"sync"
 
 	"github.com/gin-gonic/gin"
@@ -18,9 +21,20 @@ func NewGatewayHandler() *GatewayHandler {
 
 func (h *GatewayHandler) HandleRequest(c *gin.Context) {
 	path := c.Request.URL.Path
-	fmt.Println(path)
 
-	proxy := &httputil.ReverseProxy{}
+	targetURL := fmt.Sprintf("http://%s:%d", "localhost", 9090)
+	target, err := url.Parse(targetURL)
+	if err != nil {
+		log.Println(err)
+	}
+	proxy := &httputil.ReverseProxy{
+		Director: func(req *http.Request) {
+			req.URL.Scheme = target.Scheme
+			req.URL.Host = target.Host
+			req.URL.Path = path
+			req.Host = target.Host
+		},
+	}
 
 	proxy.ServeHTTP(c.Writer, c.Request)
 }
